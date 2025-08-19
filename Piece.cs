0558c1c5a7ef14381d7949f2e71c279efefe3009
Kutman878 <abdalievkutman2@gmail.com>
2025-08-19 21:49:14 +0600

    Добавлен родительский Класс Piece

--- Piece.cs
+++ Piece.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Chess_WPF
 {
-    internal class Piece
+    public abstract class Piece
     {
+        public enum Color
+        {
+            White,
+            Black
+        }
+
+        public Color PieceColor { get; set; }
+        public (int Row, int Col) Position { get; set; } // позиция на доске
+        public string Name { get; protected set; }
+
+        public Piece(Color color, int row, int col)
+        {
+            PieceColor = color;
+            Position = (row, col);
+        }
+
+        // абстрактный метод для проверки допустимых ходов
+        public abstract bool IsValidMove(int targetRow, int targetCol, Piece[,] board);
+
+        // перемещение фигуры
+        public void Move(int targetRow, int targetCol)
+        {
+            Position = (targetRow, targetCol);
+        }
+
+        public override string ToString()
+        {
+            return $"{PieceColor} {Name}";
+        }
     }
+
 }
